4e225c6af569eae52a3f19d2e0753238b90ce3d3
lihui-bubai <128445735+lihui-bubai@users.noreply.github.com>
2023-03-21 18:30:09 +0800

    --story=1017217 --user=李辉 创建一个技术债test2 https://www.tapd.cn/39091999/s/1000939

---
+++ print file.py
@@ -0,0 +1,12 @@
+string_1 = "My name is Chaitanya Baweja"
+string_2 = "sample/ string 2"
+
+
+# default separator
+print(string_1.split())
+# [ My , name , is , Chaitanya , Baweja ]
+
+
+# defining separator as /
+print(string_2.split( / ))
+# [ sample , string 2 ]
