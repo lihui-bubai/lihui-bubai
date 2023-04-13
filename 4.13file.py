4b1ad5d3c2351ad7a1c317939814bea36a7957e4
lihui-bubai <128445735+lihui-bubai@users.noreply.github.com>
2023-04-13 10:03:23 +0800

    --bug=1007978 --user=李辉 4.12新bug https://www.tapd.cn/39091999/s/1001291

--- 4.13file.py
+++ 4.13file.py
@@ -1,21 +1,17 @@
-import argparse
+# _*_ encoding: utf-8 _*_   @author: ty  hery   2019/2/28
+def spread(arg):
+    ret = []
+    for i in arg:
+        if isinstance(i, list):
+            ret.extend(i)
+    else:
+        ret.append(i)
+    return ret
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--lr', default=0.001, help='learning rate', type=float)
-parser.add_argument('--batch_size', default=2048, help='batch size', type=int)
-parser.add_argument('--test_batch_size', default= 1, help='batch size', type=int)
-parser.add_argument('--number_sample', default= 1000, help='negative sampling number', type=int)
-parser.add_argument('--top_folder', default="/home/chao/haoyu/dien-taobao/", help='top folder') #"/home/chao/haoyu/dien-nsma/" #"/cluster/home/it_stu110/rec/dien-nsma" 
-parser.add_argument('--model_type', default="DIN", help='model name')
-parser.add_argument('--seed', default= 3, help='seed', type=int)
-parser.add_argument('--train_rounds', default= 4, help='seed', type=int)
-parser.add_argument('--embed_size', default= 18, help='embed size', type=int)
-parser.add_argument('--test_iter', default= 50, help='test iterations', type=int)
-parser.add_argument('--save_iter', default= 50, help='save iterations', type=int)
-parser.add_argument('--should_train', action='store_true', help='train model')
-parser.add_argument('--should_test', action='store_true', help='eval model')
-
-parser.add_argument('--dataset', default="taobao", help='dataset')
-
-args = parser.parse_args()
+def deep_flatten(lst):
+    result = []
+    result.extend(
+        spread(list(map(lambda x: deep_flatten(x) if type(x) == list else x, lst))))
+    return result
+deep_flatten([1, [2], [[3], 4], 5])  # [1,2,3,4,5]
+print(deep_flatten([1, [2], [[3], 4], 5]))  # [1,2,3,4,5]
