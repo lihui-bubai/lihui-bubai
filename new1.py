9c53415f481dd845e910aff763d0db6e7d2e40d1
lihui-bubai <128445735+lihui-bubai@users.noreply.github.com>
2023-09-11 14:19:31 +0800

    Create new1.py

---
+++ new1.py
@@ -0,0 +1,22 @@
+# 输出 Hello, world!
+print("Hello, world!")
+# 声明变量并赋值
+x = 5
+y = "John"
+print(x)
+print(y)
+# 循环
+for i in range(1, 6):
+print(i)
+# 判断语句
+if x >10:
+print("x is greater than 10")
+else:
+print("x is less than or equal to 10")
+# 函数
+def greet(name):
+print("Hello, " + name + "!")
+greet("John")
+# 导入模块
+import random
+print(random.randint(1, 10))
